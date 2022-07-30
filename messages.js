13362a0745477e499bcfb15ca4f15a94882677b3
Stefanie Harris <you@example.com>
2022-07-29 22:50:45 -0700

    Add beginnings of message generator, first basic function

--- messages.js
+++ messages.js
@@ -1,1 +1,13 @@
 console.log(`hello, I don't remember anything anymore`)
+
+function returnFortune(string){
+    if (string === `Tell me my fortune`){
+        return 
+    }else{
+        return `Please ask for your fortune correctly.`
+    }
+}
+
+function randomFirstWord(){
+    
+}
