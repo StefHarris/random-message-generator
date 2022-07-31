34a8c12367892465a16bfd9cc2c7906795033c23
Stefanie Harris <you@example.com>
2022-07-31 09:49:30 -0700

    Add comments and plans for how to create the scripts

--- messages.js
+++ messages.js
@@ -2,12 +2,17 @@ console.log(`hello, I don't remember anything anymore`)
 
 function returnFortune(string){
     if (string === `Tell me my fortune`){
-        return 
+        return `Your fortune is: `
     }else{
         return `Please ask for your fortune correctly.`
     }
 }
 
 function randomFirstWord(){
-    
+
 }
+
+//assign a number to a word then do the math floor random thing, then another function if the number = that then it returns the word
+//random word 1, random word 2, random word 3, each one uses the math random floor and generates a number between 1-3, one of the 
+//parameters = the number, then if the number = 1, return whatever
+//in the returnFunction put all the random word functions together 
