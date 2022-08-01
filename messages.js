f3cfda03af20b5f387c9810b34df98aa3687ea6c
Stefanie Harris <you@example.com>
2022-07-31 22:39:26 -0700

    Fix typos and add ideas for possible fortunes

--- messages.js
+++ messages.js
@@ -9,12 +9,14 @@ function returnFortune(string){
 }
 
 function randomFirstWord(){
-    let firstWord = [`You`, `Your friends`,`Your pet`, `Your plants`]
+    let firstWord = [`You`, `Your friends`,`Your pets`, `Your plants`]
     let randomNumber = Math.floor(Math.random()*firstWord.length)
     return firstWord[randomNumber]
     }
     
 
+//word two: are, will be, might be
+//word three: conspiring against you, rich, destitute, happy just as you are
 //assign a number to a word then do the math floor random thing, then another function if the number = that then it returns the word
 //random word 1, random word 2, random word 3, each one uses the math random floor and generates a number between 1-3, one of the 
 //parameters = the number, then if the number = 1, return whatever
