b6ff60a6bbd6ac087dd3283f111cf9054b807f71
Stefanie Harris <you@example.com>
2022-07-31 23:59:41 -0700

    completed random message generator, now gives a random three part fortune ranging from positive to bleak

--- messages.js
+++ messages.js
@@ -1,8 +1,8 @@
-console.log(`hello, I don't remember anything anymore`)
+console.log(`hello, ask for your fortune... at your own peril.`)
 
 function returnFortune(string){
     if (string === `Tell me my fortune`){
-        return `Your fortune is: `
+        return `Your fortune is: ${randomFirstWord()} ${randomSecondWord()} ${randomThirdWord()}.`
     }else{
         return `Please ask for your fortune correctly.`
     }
@@ -13,7 +13,18 @@ function randomFirstWord(){
     let randomNumber = Math.floor(Math.random()*firstWord.length)
     return firstWord[randomNumber]
     }
-    
+function randomSecondWord(){
+    let secondWord = [`are`, `will be`, `might be`]
+    let randomNumber = Math.floor(Math.random()*secondWord.length)
+    return secondWord[randomNumber]
+}
+function randomThirdWord(){
+    let thirdWord = [`conspiring against you`, `rich`, `destitute`, `happy just as you are`]
+    let randomNumber = Math.floor(Math.random()*thirdWord.length)
+    return thirdWord[randomNumber]
+}
+
+console.log(returnFortune(`Tell me my fortune`))
 
 //word two: are, will be, might be
 //word three: conspiring against you, rich, destitute, happy just as you are
@@ -21,3 +32,5 @@ function randomFirstWord(){
 //random word 1, random word 2, random word 3, each one uses the math random floor and generates a number between 1-3, one of the 
 //parameters = the number, then if the number = 1, return whatever
 //in the returnFunction put all the random word functions together 
+
+//maybe come back to add html and css to make it look like an actual website where you put the request in the box...
